e407cf3d30c2ce5155bb4325b64d67523079129d
Albin Varghese <albin8285@gmail.com>
2021-07-24 13:39:57 +0530

    phpinsights-fix

--- app/Commands/UploadCommand.php
+++ app/Commands/UploadCommand.php
@@ -1,18 +1,15 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Commands;
 
+use App\Helpers\Anonfiles;
 use Illuminate\Console\Scheduling\Schedule;
 use LaravelZero\Framework\Commands\Command;
-use Storage;
-use GuzzleHttp\Psr7;
-use GuzzleHttp\Client;
-use GuzzleHttp\Psr7\Request;
-use App\Helpers\Anonfiles;
 
 class UploadCommand extends Command
 {
-	
     /**
      * The signature of the command.
      *
@@ -27,108 +24,91 @@ class UploadCommand extends Command
      * @var string
      */
     protected $description = 'Upload files to anonfiles';
-    
+
     protected $disk;
-    
+
     public function __construct()
     {
-    	parent::__construct();
-	    
-		// initialize Anonfiles helper.
-    	$this->anonfiles = new Anonfiles();
-	    
+        parent::__construct();
+
+        // initialize Anonfiles helper.
+        $this->anonfiles = new Anonfiles();
     }
-    
-    
+
     /**
      * Execute the console command.
-     *
-     * @return mixed
      */
-    public function handle()
+    public function handle(): mixed
     {
-    	// show logo.
-	    $this->anonfiles->logo('Anonfiles', 'comment');
+        // show logo.
+        $this->anonfiles->logo('Anonfiles', 'comment');
 
-    	// create new disk instance.
-	    $this->anonfiles->createDisk();
-		
-		
-		$this->file = $this->argument('filename');
-		
-		// validate the file before uploading.
-		$this->validate();
-		
-		//
-	    $this->showFileMetaData();
-        
-        if($this->confirm('Do you want to upload file?')){
-        	try {
-	        	$this->anonfiles->upload();
-			} catch(\GuzzleHttp\Exception\ConnectException $e) {
-				$this->error($e->getMessage());
-			} catch(\Exception $e) {
-				$this->error($e->getMessage());
-			}
-        };
-        
-        
+        // create new disk instance.
+        $this->anonfiles->createDisk();
+
+        $this->file = $this->argument('filename');
+
+        // validate the file before uploading.
+        $this->validate();
+
+        $this->showFileMetaData();
+
+        if ($this->confirm('Do you want to upload file?')) {
+            try {
+                $this->anonfiles->upload();
+            } catch (\GuzzleHttp\Exception\ConnectException $e) {
+                $this->error($e->getMessage());
+            } catch (\Exception $e) {
+                $this->error($e->getMessage());
+            }
+        }
+
         $this->showResponse();
     }
-    
-    
-    private function validate()
+
+    public function showResponse(): void
     {
-    	if(!$this->anonfiles->checkIfFileExists($this->file))
-	    {
-			$this->error("File doesn't exist.");
-			exit();
-		}
-		
-		$this->anonfiles->setFile($this->file);
-		
+        $json = $this->anonfiles->getResponse();
+
+        if ($json->status) {
+            $this->comment('   File uploaded ✅');
+            $this->newline();
+            $this->info('link : '. $json->data->file->url->full);
+            $this->newline();
+        } else {
+            $this->error = 'Uploading failed...';
+        }
     }
-    
-    private function showFileMetaData()
-    {
-    	$headers = ['Properties', 'Values'];
-    
-    	$data = [
-		    ['filename', $this->anonfiles->getFileName()],
-	        ['path', $this->anonfiles->path],
-	        ['size', $this->anonfiles->getSize()],
-			['last modified', $this->anonfiles->getLastModified()],
-	    ];
-    
-	    $this->table($headers, $data);
-    }
-    
-    
-    
-    public function showResponse()
-    {
-    	$json = $this->anonfiles->getResponse();
-	    
-		if($json->status)
-		{
-		    $this->comment('   File uploaded ✅');
-			$this->newline();
-			$this->info('link : '. $json->data->file->url->full);
-			$this->newline();
-		} else {
-			$this->error = "Uploading failed...";
-		}
-    }
-    
-    
+
     /**
      * Define the command's schedule.
-     *
-     * @param  \Illuminate\Console\Scheduling\Schedule $schedule
-     * @return void
      */
     public function schedule(Schedule $schedule): void
     {
         // $schedule->command(static::class)->everyMinute();
+    }
+
+    private function validate(): void
+    {
+        if (! $this->anonfiles->checkIfFileExists($this->file)) {
+            $this->error("File doesn't exist.");
+            exit;
+        }
+
+        $this->anonfiles->setFile($this->file);
+    }
+
+    private function showFileMetaData(): void
+    {
+        $headers = ['Properties', 'Values'];
+
+        $data = [
+            ['filename', $this->anonfiles->getFileName()],
+            ['path', $this->anonfiles->path],
+            ['size', $this->anonfiles->getSize()],
+            ['last modified', $this->anonfiles->getLastModified()],
+        ];
+
+        $this->table($headers, $data);
     }
 }
--- app/Helpers/Anonfiles.php
+++ app/Helpers/Anonfiles.php
@@ -1,144 +1,138 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Helpers;
 
+use GuzzleHttp\Client;
+use GuzzleHttp\Psr7;
+use GuzzleHttp\Psr7\Request;
+use Laminas\Text\Figlet\Figlet;
 use LaravelZero\Framework\Commands\Command;
 use Storage;
-use Laminas\Text\Figlet\Figlet;
-use GuzzleHttp\Psr7;
-use GuzzleHttp\Client;
-use GuzzleHttp\Psr7\Request;
 
 class Anonfiles extends Command
 {
-	
-	public $disk;
-	
-	public function __construct()
-	{
-		$this->output = new \Symfony\Component\Console\Output\ConsoleOutput();
-	}
-	
-	public function logo($name = "Anonfiles CLI", $type = null, $font = null): void
+    public $disk;
+
+    public function __construct()
+    {
+        $this->output = new \Symfony\Component\Console\Output\ConsoleOutput();
+    }
+
+    public function logo($name = 'Anonfiles CLI', $type = null, $font = null): void
     {
         $name = is_null($name) ? config('logo.name') : $name;
         $font = is_null($font) ? config('logo.font') : $font;
-
         $figlet = new Figlet();
         $logo = $figlet->setFont($font)->render($name);
-
         switch ($type) {
             case 'info':
-                $this->info($logo);
-                break;
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     $this->info($logo);
 
-            case 'error':
-                $this->error($logo);
                 break;
+case 'error':
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     $this->error($logo);
 
-            case 'comment':
-                $this->comment($logo);
-                break;
+    break;
+case 'comment':
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     $this->comment($logo);
 
-            case 'question':
-                $this->question($logo);
-                break;
+    break;
+case 'question':
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     $this->question($logo);
 
-            default:
-                $this->line($logo);
-                break;
-        }
+    break;
+default:
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                     $this->line($logo);
+
+    break;
+
+}
     }
 
     public function clear(): mixed
     {
         return system('clear');
     }
-	
-	public function setFile($file)
-	{
-		$this->file = $file;
-		$this->path = $this->disk->path($file);
-		$this->getMetaData();
-	}
-	
-	public function createDisk()
-	{
-		try {
-			$this->disk = Storage::build(['driver' => 'local', 'root' => getcwd()]);
-			return true;
-		} catch(\Exception $e) {
-			return false;
-		}
-	}
-	
-	public function getFilename()
-	{
-		return basename($this->path);
-	}
-	
-	public function validate()
-	{
-		//
-	}
-	
-	private function getMetadata()
-	{
-		$this->fileLastModified = $this->disk->lastModified($this->file);
-		$this->fileSize = $this->disk->size($this->file);
-	}
-	
-	public function getSize()
-	{
-		return $this->diffForHumans($this->fileSize);
-	}
-	
-	public function getLastModified()
-	{
-		return date('m/d/Y H:i:s', $this->fileLastModified);
-	}
-	
-	public function checkIfFileExists($pathToFile)
-	{
-		if($this->disk->exists($pathToFile))
-	    {
-			return true;
-		}
-		
-		return false;
-	}
-	
-	public function diffForHumans($bytes, $dec = 2)
+
+    public function setFile($file): void
     {
-    	$size = config('anonfiles.STORAGE_UNITS');
-	    
-	    $factor = floor((strlen($bytes) - 1) / 3);
-	    return sprintf("%.{$dec}f", $bytes / pow(1024, $factor)) . @$size[$factor];
+        $this->file = $file;
+        $this->path = $this->disk->path($file);
+        $this->getMetaData();
     }
-	
-	public function upload()
+
+    public function createDisk()
     {
-    	$this->client = new Client(['http_error' => false, 'progress' => function(
+        try {
+            $this->disk = Storage::build(['driver' => 'local', 'root' => getcwd()]);
+            return true;
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+
+    public function getFilename()
+    {
+        return basename($this->path);
+    }
+
+    public function validate(): void
+    {
+        
+    }
+
+    public function getSize()
+    {
+        return $this->diffForHumans($this->fileSize);
+    }
+
+    public function getLastModified()
+    {
+        return date('m/d/Y H:i:s', $this->fileLastModified);
+    }
+
+    public function checkIfFileExists($pathToFile)
+    {
+        if ($this->disk->exists($pathToFile)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function diffForHumans($bytes, $dec = 2)
+    {
+        $size = config('anonfiles.STORAGE_UNITS');
+
+        $factor = floor((strlen($bytes) - 1) / 3);
+        return sprintf("%.{$dec}f", $bytes / pow(1024, $factor)) . @$size[$factor];
+    }
+
+    public function upload(): void
+    {
+        $this->client = new Client(['http_error' => false, 'progress' => function (
             $downloadTotal,
             $downloadedBytes,
             $uploadTotal,
             $uploadedBytes
-        ) {
+        ): void {
             $res = curl_getinfo($downloadTotal);
-            echo "\033[5D";      
+            echo "\033[5D";
             $msg = $this->diffForHumans($res['size_upload']) . ' / ' . $this->diffForHumans($res['upload_content_length']);
-		    echo "     📂  Progress : {$msg} \r";
-        },]);
+            echo "     📂  Progress : {$msg} \r";
+        },
+        ]);
+        $resource = $this->disk->get($this->file);
 
-    	$resource = $this->disk->get($this->file);
-	    
-		$stream = Psr7\stream_for($resource);
-		
-		$request = new Request(
-        'POST',
-        config('anonfiles.UPLOAD_ENDPOINT'),
-        [],
-        new Psr7\MultipartStream(
+        $stream = Psr7\stream_for($resource);
+
+        $request = new Request(
+            'POST',
+            config('anonfiles.UPLOAD_ENDPOINT'),
+            [],
+            new Psr7\MultipartStream(
             [
                 [
                     'name' => 'file',
@@ -147,14 +141,19 @@ class Anonfiles extends Command
                 ],
             ]
         )
-	);
-	
-	$this->response = $this->client->send($request);
+        );
+
+        $this->response = $this->client->send($request);
     }
-    
-    
+
     public function getResponse()
     {
-    	return json_decode($this->response->getBody());
+        return json_decode($this->response->getBody());
+    }
+
+    private function getMetadata(): void
+    {
+        $this->fileLastModified = $this->disk->lastModified($this->file);
+        $this->fileSize = $this->disk->size($this->file);
     }
 }
